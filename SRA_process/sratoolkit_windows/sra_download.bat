4d314aeb4fc72e8f8f998417fa1eec9a8a1f5ad5
Xianqing Jia <jiaxianqing2010@126.com>
2020-05-18 21:03:46 +0800

    Update sra_download.bat

--- SRA_process/sratoolkit_windows/sra_download.bat
+++ SRA_process/sratoolkit_windows/sra_download.bat
@@ -8,7 +8,7 @@ echo "Start procesing ... "
 echo "You can take a nap, if you want to download many files zzZ"
 echo
 
-E:\SRA_download\sratoolkit.2.9.6\bin\prefetch.exe --option-file %file% --output-directory ./public/%outfile% --verbose
+.\sratoolkit.2.9.6\bin\prefetch.exe --option-file %file% --output-directory ./public/%outfile% --verbose
 
 echo
 echo "Congratulations! All is finished! Please check the md5 values."
